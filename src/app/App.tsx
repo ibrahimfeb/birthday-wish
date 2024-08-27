c3373771d58744e1e5d22dc36ae09d66a3d87889
Muhammad Ibrahim <muhammadibrahim5644@gmail.com>
2024-08-27 13:11:43 +0500

    height full

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import Home from "../pages/Home";
 
 const App = () => {
   return (
-    <div className="bg-gradient-to-b from-custom-500 to-custom-600 via-mint-200 h-screen w-full">
+    <div className="bg-gradient-to-b from-custom-500 to-custom-600 via-mint-200 h-full w-full">
       <Home />
     </div>
   );
