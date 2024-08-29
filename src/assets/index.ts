88b3828a540be610a72c330d5aa874ccb3fe179c
Muhammad Ibrahim <muhammadibrahim5644@gmail.com>
2024-08-29 08:15:03 +0500

    update spelling

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,7 +4,7 @@ export { default as mount2 } from "../assets/images/mount2.jpg";
 export { default as coupleShape } from "../assets/images/coupleShape.png";
 export { default as shapeOne } from "../assets/images/shape-1.png";
 export { default as shapeTwo } from "../assets/images/shape-2.png";
-export { default as loveOne } from "../assets/images/LoveOne.jpg";
+export { default as loveOne } from "../assets/images/loveOne.jpg";
 export { default as loveTwo } from "../assets/images/loveTwo.jpg";
 export { default as loveThree } from "../assets/images/loveThree.jpg";
 export { default as goldenHeart } from "../assets/icons/goldenHeart.svg";
