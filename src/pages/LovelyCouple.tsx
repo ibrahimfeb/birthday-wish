326f5be27bf3610a916420d902bf298fba417e02
Muhammad Ibrahim <muhammadibrahim5644@gmail.com>
2024-09-09 15:59:55 +0500

    update code

--- src/pages/LovelyCouple.tsx
+++ src/pages/LovelyCouple.tsx
@@ -13,13 +13,41 @@ import {
 const LovelyCouple = () => {
   return (
     <>
+      <section
+        id="coming_soon"
+        className="coming_soon_area py-16 "
+        style={{ backgroundColor: "transparent" }}
+      >
+        <div className="coming_soon_shape_1">
+          <img src={shapeOne} alt="shape" />
+        </div>
+
+        <div className="container items-center  ">
+          <div className="text-center">
+            <h1 className="pb-[5px]">Happy Birthday!</h1>
+            <p style={{ color: "#D57282", textAlign: "center" }}>
+              On this special day, I want to take a moment to express just how
+              much you mean to me. Happy birthday to the love of my life, my
+              partner, my confidante, and my best friend! Each year, as we
+              celebrate your birthday, I am reminded of how fortunate I am to
+              have you by my side. You are not just my wife; you are the light
+              of my life, the reason behind my smile, and the source of my
+              happiness.
+            </p>
+          </div>
+        </div>
+
+        <div className="coming_soon_shape_2">
+          <img src={shapeTwo} alt="shape" className="mt-[70px]" />
+        </div>
+      </section>
+
       <section id="couple" className="couple_area pt-[20px] pb-130">
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-lg-6">
               <div className="section_title text-center pb-30 flex flex-col items-center justify-center">
-                <h3 className="title">Lovely Couple</h3>
-                <img src={section_shape} alt="Shape" className="self-center" />
+                {/* <h3 className="title">Lovely Couple</h3> */}
               </div>
             </div>
           </div>
@@ -35,13 +63,14 @@ const LovelyCouple = () => {
                 </div>
                 <div className="couple_content text-center">
                   <img className="shape" src={coupleShape} alt="shape" />
-                  <h4 className="couple_name">Michael  &nbsp; Smith</h4>
+                  <h4 className="couple_name">Michael &nbsp; Smith</h4>
                   <p>
                     Hi I am Michael Smith, dummy text of the printing and
                     typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since &nbsp; the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries.
+                    standard dummy text ever since &nbsp; the 1500s, when an
+                    unknown printer took a galley of type and scrambled it to
+                    make a type specimen book. It has survived not only five
+                    centuries.
                   </p>
                   &nbsp;
                 </div>
@@ -62,9 +91,10 @@ const LovelyCouple = () => {
                   <p>
                     Hi I am Jessica Jones, dummy text of the printing and
                     typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since &nbsp; the 1500s, when an unknown
-                    printer took a galley of type and scrambled it to make a
-                    type specimen book. It has survived not only five centuries.
+                    standard dummy text ever since &nbsp; the 1500s, when an
+                    unknown printer took a galley of type and scrambled it to
+                    make a type specimen book. It has survived not only five
+                    centuries.
                   </p>
                   &nbsp;
                 </div>
@@ -73,7 +103,7 @@ const LovelyCouple = () => {
           </div>
         </div>
       </section>
-
+      {/* 
       <section id="coming_soon" className="coming_soon_area py-16 ">
         <div className="coming_soon_shape_1">
           <img src={shapeOne} alt="shape" />
@@ -82,7 +112,7 @@ const LovelyCouple = () => {
         <div className="container items-center  ">
           <div className="text-center">
             <h1 className="pb-[5px]">Happy Birthday!</h1>
-            <p>
+            <p style={{ color: "#D57282" }}>
               Wishing you a day filled with joy, laughter, and love. May all
               your wishes come true. Happy Birthday!
             </p>
@@ -92,7 +122,7 @@ const LovelyCouple = () => {
         <div className="coming_soon_shape_2">
           <img src={shapeTwo} alt="shape" />
         </div>
-      </section>
+      </section> */}
 
       <section id="our_love" className="our_love_area pt-60">
         <div className="container">
@@ -100,7 +130,7 @@ const LovelyCouple = () => {
             <div className="col-lg-6">
               <div className="section_title text-center pb-30 flex flex-col items-center justify-center">
                 <h3 className="title">Our Love Story</h3>
-                <img src={section_shape} alt="Shape" className="self-center" />
+                {/* <img src={section_shape} alt="Shape" className="self-center" /> */}
               </div>
             </div>
           </div>
@@ -111,14 +141,16 @@ const LovelyCouple = () => {
                 data-wow-duration="1.3s"
                 data-wow-delay="0.4s"
               >
-                <h3 className="love_title">We Met For The First Time</h3>
+                <h3 className="love_title">From the moment we met</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                  nose aaliquyam erat, sed diam voluptua. At vero eos et accusam
-                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-                  sea timata sanctus est Lorem ipsum dolor sit amet. Lorem
-                  ipsum.
+                  I knew there was something extraordinary about you. Your
+                  kindness, warmth, and beauty captivated me instantly. You have
+                  a way of making every day brighter and every moment more
+                  meaningful. Your laughter is like music to my ears, and your
+                  smile has the power to lift my spirits even on the toughest
+                  days. You have an incredible ability to turn ordinary moments
+                  into extraordinary memories, and I cherish every single one of
+                  them
                 </p>
               </div>
               <div className="love_date">
@@ -142,14 +174,16 @@ const LovelyCouple = () => {
                 data-wow-duration="1.3s"
                 data-wow-delay="0.4s"
               >
-                <h3 className="love_title">We AreFirst Dating</h3>
+                <h3 className="love_title">
+                  I am grateful for the love and support
+                </h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                  nose aaliquyam erat, sed diam voluptua. At vero eos et accusam
-                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-                  sea timata sanctus est Lorem ipsum dolor sit amet. Lorem
-                  ipsum.
+                  You have been my rock during difficult times, always
+                  encouraging me to pursue my dreams and reminding me of my
+                  worth. Your belief in me has given me the courage to chase my
+                  goals and strive for greatness. I want you to know that I will
+                  always be here for you, just as you have been there for me.
+                  Together, we can conquer anything that comes our way.
                 </p>
               </div>
               <div className="love_date">
@@ -173,14 +207,18 @@ const LovelyCouple = () => {
                 data-wow-duration="1.3s"
                 data-wow-delay="0.4s"
               >
-                <h3 className="love_title">We Live Together</h3>
+                <h3 className="love_title">
+                  As we celebrate your birthday today
+                </h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                  diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                  nose aaliquyam erat, sed diam voluptua. At vero eos et accusam
-                  justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-                  sea timata sanctus est Lorem ipsum dolor sit amet. Lorem
-                  ipsum.
+                  I want you to know how deeply I admire you. You are a
+                  remarkable woman with so many talents and qualities that
+                  inspire me every day. Your strength and resilience in the face
+                  of challenges are nothing short of awe-inspiring. You handle
+                  every situation with grace and poise, and I am constantly
+                  amazed by your ability to find joy in the little things. You
+                  teach me to appreciate life and to embrace every moment with
+                  an open heart.
                 </p>
               </div>
               <div className="love_date">
@@ -201,11 +239,11 @@ const LovelyCouple = () => {
       <section id="gallery" className="gallery_area pt-80">
         <div className="container">
           <div className="text-center">
-            <h1 className="pb-[5px]">Always be Happy</h1>
-            <p>
+            <h1 className="pb-[5px]"></h1>
+            {/* <p style={{ color: "#D57282" }}>
               You deserve the best day ever. I hope it's filled with love,
               laughter, and everything you desire.
-            </p>
+            </p> */}
           </div>
         </div>
       </section>
@@ -215,7 +253,7 @@ const LovelyCouple = () => {
           <div className="row justify-content-center">
             <div className="col-lg-6">
               <div className="section_title text-center pb-30 flex flex-col items-center justify-center">
-                <h3 className="title">Birthday Events</h3>
+                <h3 className="title">Our Moments</h3>
                 <img src={section_shape} alt="Shape" className="self-center" />
               </div>
             </div>
@@ -233,13 +271,9 @@ const LovelyCouple = () => {
                 <div className="event_content">
                   <span className="date">02 Feb 2023</span>
                   <h3 className="event_title">
-                    <p>Photography</p>
+                    <p>First We Met at Gurdwara</p>
                   </h3>
-                  <p>
-                    Typesetting industry. Lorem Ipsum has been the industry's
-                    standard dummy text ever since the 1500s, when unknown
-                    printer took a galley
-                  </p>
+                  {/* s */}
                 </div>
               </div>
             </div>
@@ -255,13 +289,13 @@ const LovelyCouple = () => {
                 <div className="event_content">
                   <span className="date">05 Feb 2023</span>
                   <h3 className="event_title">
-                    <p>Dancing</p>
+                    <p>Monal Restaurent Lunch</p>
                   </h3>
-                  <p>
+                  {/* <p>
                     Typesetting industry. Lorem Ipsum has been the industry's
                     standard dummy text ever since the 1500s, when unknown
                     printer took a galley
-                  </p>
+                  </p> */}
                 </div>
               </div>
             </div>
@@ -279,11 +313,11 @@ const LovelyCouple = () => {
                   <h3 className="event_title">
                     <p>Movie Plan</p>
                   </h3>
-                  <p>
+                  {/* <p>
                     Typesetting industry. Lorem Ipsum has been the industry's
                     standard dummy text ever since the 1500s, when unknown
                     printer took a galley
-                  </p>
+                  </p> */}
                 </div>
               </div>
             </div>
@@ -296,7 +330,8 @@ const LovelyCouple = () => {
           <div className="text-center">
             <h1 className="pb-[5px]">Have a bright future</h1>
             <p>
-            Wishing you a lifetime of happiness and joy. I'm so grateful to have you in my life.
+              Wishing you a lifetime of happiness and joy. I'm so grateful to
+              have you in my life.
             </p>
           </div>
         </div>
@@ -304,28 +339,33 @@ const LovelyCouple = () => {
 
       <footer id="footer" className="footer_area mt-80">
         <div className="footer_shape_1">
-            <img src={shapeOne} alt="shape"/>
+          <img src={shapeOne} alt="shape" />
         </div>
-        
-        <div className="container">
-            <div className="footer_widget pt-80 pb-80 text-center">
-                
-                <div className="footer_title">
-                    <h3 className="title">Michael <span>&</span> Jessica</h3>
-                </div>
-                <ul className="footer_menu">
-                    <li>Love</li>
-                    <li>Joy</li>
-                    <li>Happiness</li>
-                    <li>❤️</li>
-                </ul>
-            </div> 
-            <div className="footer_copyright text-center">
-                <p>Designed and Developed by <p  style={{color: "#D59A57"}} rel="nofollow">M Ibrahim</p></p>
-            </div>
-        </div> 
-    </footer>
 
+        <div className="container">
+          <div className="footer_widget pt-80 pb-80 text-center">
+            <div className="footer_title">
+              <h3 className="title" style={{ color: "#c43451" }}>
+                Michael Jessica
+              </h3>
+            </div>
+            <ul className="footer_menu">
+              <li>Love</li>
+              <li>Joy</li>
+              <li>Happiness</li>
+              <li>❤️</li>
+            </ul>
+          </div>
+          <div className="footer_copyright text-center">
+            <p>
+              Designed and Developed by{" "}
+              <p style={{ color: "#D59A57" }} rel="nofollow">
+                M Ibrahim
+              </p>
+            </p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
